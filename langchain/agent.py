7cc9e68ddf0b8c6fa5a2eeadcefe83269e258405
Alex Liu <bronzels@hotmail.com>
2026-01-11 12:59:39 +0800

    same to last

--- langchain/agent.py
+++ langchain/agent.py
@@ -341,3 +341,15 @@ len(result['messages']) ==  1
 ]
 ```
 """
+
+
+"""
+$  cd d:\\shouxieagent ; /usr/bin/env d:\\conda_envs\\shouxieagent\\python.exe c:\\Users\\Zac\ Liu\\.vscode\\extensions\\ms-python.debugpy-2025.18.0-win32-x64\\bundled\\libs\\debugpy\\launcher 62299 -- d:\\shouxieagent\\langchain\\agent_debug5_handler_callback.py 
+[DEBUG] route_after_agent: stage=None, has_tool_calls=True
+[DEBUG] update_stage: search_doc
+[DEBUG] route_after_agent: stage=search_doc, has_tool_calls=True
+[DEBUG] update_stage: analyze_data
+[DEBUG] route_after_agent: stage=analyze_data, has_tool_calls=True
+[DEBUG] update_stage: generate_report
+[DEBUG] route_after_agent: stage=generate_report, has_tool_calls=False
+"""
